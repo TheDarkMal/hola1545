989e502f2d06dd12f606154c94335be24d3e0834
jhoinner <jhoinnerballestas@gmail.com>
2019-06-20 14:59:39 -0500

    hola1545

---
+++ src/hola1545/Hola1545.java
@@ -0,0 +1,15 @@
+
+package hola1545;
+
+ import java.util.Scanner;
+public class Hola1545 {
+   public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        String mensaje;
+        System.out.println("Digite un texto");
+        mensaje = leer.nextLine();
+        
+        System.out.println(mensaje);
+    }
+    
+}
